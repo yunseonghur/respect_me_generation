59f7bfbb4fb204cef709d29b18014addc52beba7
Gina <ginakim.jh@gmail.com>
2020-04-24 16:55:31 -0700

    added firebase configuration

---
+++ src/fire.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC4AYpNhs6fcMlJEIryxi4KkOAcvTdgB_Y",
+    authDomain: "project3800-test.firebaseapp.com",
+    databaseURL: "https://project3800-test.firebaseio.com",
+    projectId: "project3800-test",
+    storageBucket: "project3800-test.appspot.com",
+    messagingSenderId: "801608378603",
+    appId: "1:801608378603:web:77485eac7b3e46ca03f6ae"
+};
+const fire = firebase.initializeApp(firebaseConfig);
+
+export default fire;

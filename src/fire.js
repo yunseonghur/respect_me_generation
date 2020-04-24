a54f5bd2f766f001b96ade67a850fc706c4c7458
Sherry <sherryxrg@outlook.com>
2020-04-24 16:49:04 -0700

    added firebase integration

---
+++ src/fire.js
@@ -0,0 +1,13 @@
+
+
+  var firebaseConfig = {
+    apiKey: "AIzaSyC4AYpNhs6fcMlJEIryxi4KkOAcvTdgB_Y",
+    authDomain: "project3800-test.firebaseapp.com",
+    databaseURL: "https://project3800-test.firebaseio.com",
+    projectId: "project3800-test",
+    storageBucket: "project3800-test.appspot.com",
+    messagingSenderId: "801608378603",
+    appId: "1:801608378603:web:77485eac7b3e46ca03f6ae"
+  };
+  // Initialize Firebase
+  //firebase.initializeApp(firebaseConfig);

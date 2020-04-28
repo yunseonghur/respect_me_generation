dd504a4592a5314b5212ac57489c7cdef86829cc
hsiehii <hsiehiisuan@gmail.com>
2020-04-28 13:50:04 -0700

    package-lock

--- src/routes/Cards.js
+++ src/routes/Cards.js
@@ -1,13 +1,27 @@
 import React from 'react';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 // import "./Cards.css";
 
-// class Cards extends React.Component{
-// }
+class Card extends React.Component{
+    render() {
+        return (
+            <Card style={{width: '18rem'}}>
+            <Button variant="primary">
+                <Card.Img variant="top" src="https://i.pinimg.com/564x/83/f0/3a/83f03ac706568420b12b98ed22016650.jpg" />
+            </Button>
+            <Card.Body>
+                <Card.Title>Owner</Card.Title>
+            </Card.Body>
+        </Card>
+        )
+    }
+}
 
 function Cards(){
     return (
         <div>
-            <span>This is a page for Cards</span>
+            <ExampleCard></ExampleCard>
         </div>
     );
 }

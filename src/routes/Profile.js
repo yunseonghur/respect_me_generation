87f60f53726c0a66a74145cbbc204342e284eca5
Yuni <yshuh0126@gmail.com>
2020-04-29 11:00:22 -0700

    Initializing points and badge works

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -5,17 +5,45 @@ import MyCard from '../components/MyCard';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import fire from '../fire.js';
+
 
 class Profile extends React.Component{
     state = {
-        username: "Yuni" // need to read it from firebase
+        userUID: "",
+        username: "",
+        badge: "",
+        points: "",
+        cards: [],
+        videos: []
     };
+    componentDidMount(){
+        fire.auth().onAuthStateChanged((user) => {
+            if (user) {
+                this.setState({
+                    username: user.displayName,
+                    userUID: user.uid
+                });
+            } else {
+                console.log("no current user");
+            }
+        })
+        this.database = fire.database().ref().child('User').orderByKey();
+        this.database.on('value', snap => {
+            const userInfo = snap.val();
+            this.setState({
+                badge: userInfo[this.state.userUID]['badge'],
+                points: userInfo[this.state.userUID]['points']
+            });
+        });
+    }
     render(){
         return (
             <div>
-                <h2 className="logo-text"><b>Respect Me<br/>Generation</b></h2>
                 <Jumbotron>
                     <p>Hi {this.state.username}!</p>
+                    <span>badge: {this.state.badge}, </span>
+                    <span>points: {this.state.points}</span>
                 </Jumbotron>
 
                 <Container>

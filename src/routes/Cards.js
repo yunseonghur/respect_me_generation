303fe57c2e6a240fda01976c1e9f463f9e059ae5
hsiehii <hsiehiisuan@gmail.com>
2020-04-28 18:04:29 -0700

    cards

--- src/routes/Cards.js
+++ src/routes/Cards.js
@@ -1,31 +1,38 @@
-import React from 'react';
+import React, {Component} from 'react';
+import MyCard from '../components/MyCard';
+import Modal from 'react-bootstrap/Modal';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 // import "./Cards.css";
 
-class Card extends React.Component{
+class Cards extends React.Component{
+
     render() {
         return (
-            <Card style={{width: '18rem'}}>
+            <div className="deck">
+                <Card style={{width: '18rem'}}>
+                <Button variant="primary">
+                    <Card.Img variant="top" src="https://i.pinimg.com/564x/83/f0/3a/83f03ac706568420b12b98ed22016650.jpg" />
+                </Button>
+                <Card.Body>
+                    <Card.Title>Owner</Card.Title>
+                </Card.Body>
+                </Card>
+                {/* <MyCard id="1" background="https://via.placeholder.com/120px100" text="You yourself, as much as anybody in the entire universe, deserve your love and affection" />
+                <MyCard id="2" background="https://via.placeholder.com/120px100" text="Life is just about choices." /> */}
+            
+            </div>
+        )
+    }
+}
+
+{/* <Card style={{width: '18rem'}}>
             <Button variant="primary">
                 <Card.Img variant="top" src="https://i.pinimg.com/564x/83/f0/3a/83f03ac706568420b12b98ed22016650.jpg" />
             </Button>
             <Card.Body>
                 <Card.Title>Owner</Card.Title>
             </Card.Body>
-        </Card>
-        )
-    }
-}
-
-function Cards(){
-    return (
-        <div>
-            <ExampleCard></ExampleCard>
-        </div>
-    );
-}
-
-
+            </Card> */}
 
 export default Cards;

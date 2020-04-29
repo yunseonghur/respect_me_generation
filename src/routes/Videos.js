3dec349264bc7adb722042069f21a8f1b0690333
Sherry <sherryxrg@outlook.com>
2020-04-28 17:53:51 -0700

    switching to dev

--- src/routes/Videos.js
+++ src/routes/Videos.js
@@ -1,14 +1,11 @@
 import React from 'react'
 import PostForm from '../components/PostForm'
 
-// write components somewhere else...
-// use 'Videos' for rendering whole page only!!
-
 function Videos(){
     return (
         <div>
             <h1>Upload a Video</h1>
-            <PostForm />
+            
         </div>
     );
 }
@@ -16,4 +13,3 @@ function Videos(){
 export default Videos;
 
 
-// vimeo access token: f45d721eef18fb0613438b1344592716

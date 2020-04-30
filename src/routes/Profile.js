1a8173b66d024f3023cc1f60deb562c3727d700b
Yuni <yshuh0126@gmail.com>
2020-04-29 23:38:32 -0700

    Retrieve a current user's cards from firebase and display on the profile page

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -6,8 +6,8 @@ import CardDeck from 'react-bootstrap/CardDeck';
 import Button from 'react-bootstrap/Button';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import fire from '../fire.js';
-import { CardHeader } from 'react-bootstrap/Card';
 
+const dbRef = fire.database().ref();
 
 class Profile extends React.Component{
     state = {
@@ -17,8 +17,24 @@ class Profile extends React.Component{
         points: "",
         cards: [],
         videos: [],
+        isLoading: true,
         visible: true  // true if cards are visible & false if videos are visible
     };
+    getCardDetails(){
+        let cards = this.state.cards;
+        let cardDetails = [];
+        for (let card in cards){
+            cardDetails.push({
+                id: card,
+                background: cards[card].imgOption,
+                text: cards[card].text
+            });
+        }
+        this.setState({
+            cards: cardDetails,  // re-set cards as an array
+            isLoading: false
+        });
+    }
     componentDidMount(){
         fire.auth().onAuthStateChanged((user) => {
             if (user) {
@@ -30,18 +46,18 @@ class Profile extends React.Component{
                 console.log("no current user");
             }
         })
-        this.database = fire.database().ref().child('User').orderByKey();
-        this.database.on('value', snap => {
+        dbRef.child('User').on('value', snap => {
             const userInfo = snap.val();
             this.setState({
                 badge: userInfo[this.state.userUID]['badge'],
-                points: userInfo[this.state.userUID]['points']
+                points: userInfo[this.state.userUID]['points'],
+                cards: userInfo[this.state.userUID]['cards']
             });
+            this.getCardDetails();
         });
     }
     render(){
         const label = this.state.visible? "Your Cards" : "Your Videos";
-
         return (
             <div>
                 <Jumbotron>
@@ -50,7 +66,7 @@ class Profile extends React.Component{
                     <span>points: {this.state.points}</span>
                 </Jumbotron>
 
-                <ButtonGroup>
+                <ButtonGroup> 
                     <Button variant="light" onClick={()=>{
                         this.setState({ visible: true});
                     }}>
@@ -62,14 +78,31 @@ class Profile extends React.Component{
                         Your Videos
                     </Button>
                 </ButtonGroup>
-                <h3>{label}</h3>
-                {this.state.visible ?
-                    <CardDeck>
-                        <MyCard id="1" background="https://via.placeholder.com/120px100" text="You yourself, as much as anybody in the entire universe, deserve your love and affection" />
-                        <MyCard id="2" background="https://via.placeholder.com/120px100" text="I have public speaking tmrw, any suggestions on how to make myself feel less anxious?" />
-                        <MyCard id="3" background="https://via.placeholder.com/120px100" text="" />
-                    </CardDeck>
-                    : <div>displaying videos</div>}
+
+                {this.state.isLoading ? (
+                    <div className="loader">
+                    <span className="loader__text">Loading...</span>
+                    </div>
+                ) : this.state.visible ?
+                    (
+                    <div className="cards">
+                        <h3>{label}</h3>
+                        <CardDeck>
+                            {this.state.cards.map((myCard)=> 
+                            <MyCard 
+                                key={myCard.id} 
+                                id={myCard.id} 
+                                background={myCard.background} 
+                                text={myCard.text} 
+                            />)}
+                        </CardDeck>
+                    </div>
+                    ) : (
+                    <div className="videos">
+                        <h3>{label}</h3>
+                        <p>displaying videos</p>
+                    </div>)
+                }
             </div>
         );
     }

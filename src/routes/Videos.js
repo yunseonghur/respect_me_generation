893288ba11edcff8ab3d8b6d7cc754d5e5bff91b
Sherry <sherryxrg@outlook.com>
2020-04-29 17:25:43 -0700

    displaying all videos function worksgiad .

--- src/routes/Videos.js
+++ src/routes/Videos.js
@@ -1,20 +1,9 @@
 import React, { Component } from 'react';
-import Uploader from "../components/Uploader";
+import VideoDisplay from "../components/VideoDisplay";
 import { Video, Transformation } from 'cloudinary-react';
-import { Player } from 'video-react';
+import axios from 'axios';
 
-function Videos(){
-    return (
-        <div>
-            <h1>Upload a Video</h1>
-            <Uploader name="sherry" age="999" />
-            <Uploader name="koof" age="888">"My longtime neighbor."</Uploader>
-            <button>Switch Name</button>
-        </div>
-    );
-}
-
-class UploadVid extends Component {
+class Videos extends Component {
 
     state ={
         cloudname: "respectmegen"
@@ -24,6 +13,7 @@ class UploadVid extends Component {
         console.log("uploadhandler was clicked");
         const myWidget = window.cloudinary.createUploadWidget({
             cloudName: this.state.cloudname, 
+            tags: ['project'],
             uploadPreset: 'h5awwspl'}, (error, result) => { 
               if (!error && result && result.event === "success") { 
                 console.log('Done! Here is the image info: ', result.info); 
@@ -33,28 +23,21 @@ class UploadVid extends Component {
         myWidget.open();
     }
 
-    showVideoHandler = () => {
-        console.log("showhandler was clicked");
-    }
-
     render() {
         return(
             <div>
                 <h1>Upload a Video</h1>
                 <button onClick={this.uploadHandler}>Upload File</button>
                 <h2>Showing all current videos</h2>
-                <button onClick={this.showVideoHandler}>Show all videos</button>
                 <div>
-                <Video controls cloudName={this.state.cloudname} publicId="ifvmawhn3oivphchla8z" >
-                    <Transformation width="511" crop="scale" />
-                </Video>
+                    <VideoDisplay />
                 </div>
             </div>
         )
     }
 }
 
-export default UploadVid;
+export default Videos;
 
 // https://234536288681363:aMMSnPSTD2UlFH35lI4uay7oOpA@api.cloudinary.com/v1_1/respectmegen/resources/video
 

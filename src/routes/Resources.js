4a931e44af345da729ffea01437c67f17ac31ef2
Sherry <sherryxrg@outlook.com>
2020-05-01 17:00:16 -0700

    added accordion component

--- src/routes/Resources.js
+++ src/routes/Resources.js
@@ -1,15 +1,40 @@
-import React from 'react';
-// import "./Resources.css";
+import React, { Component} from 'react';
+import "../routes/Resources.css";
+import Accordion from 'react-bootstrap/Accordion';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 
-// class Resources extends React.Component{
-// }
-function Resources(){
-    return (
-        <div>
-            <span>This is a page for Resources</span>
-        </div>
-    );
+class Resources extends Component{
+
+    render() {
+        return (
+            
+                <div className="base">
+                    <h1>Resources</h1>
+                    <Accordion>
+                    <Card>
+                        <Accordion.Toggle as={Card.Header} variant="link" eventKey="0">
+                            Click me!
+                        </Accordion.Toggle>
+                        <Accordion.Collapse eventKey="0">
+                            <Card.Body>Heyyyy</Card.Body>
+                        </Accordion.Collapse>
+                    </Card>
+                    <Card>
+                        <Accordion.Toggle as={Card.Header} variant="link" eventKey="1">
+                            Click me!
+                        </Accordion.Toggle>
+                        <Accordion.Collapse eventKey="1">
+                            <Card.Body>Hello! I'm the body</Card.Body>
+                        </Accordion.Collapse>
+                    </Card>
+                    </Accordion>
+                </div>
+
+        );
+    }
 }
+
 
 
 export default Resources;

0e39464cf1569021afc0c40a52d12a74b891faf1
Sherry <sherryxrg@outlook.com>
2020-05-04 16:12:38 -0700

    added resource component, reading from db successful

---
+++ src/components/ResourceEntry.js
@@ -0,0 +1,29 @@
+import React, { Component} from 'react';
+import "../routes/Resources.css";
+import Accordion from 'react-bootstrap/Accordion';
+import Card from 'react-bootstrap/Card';
+
+/**
+ * Represents a resource page entry. 
+ * 
+ * props:
+ * - title = title of the accordion fold
+ * - content = content that appears after fold is opened
+ * - eventKey = if this entry starts OPEN, match this num with the defaultActiveKey 
+ * 
+ * @param {*} props 
+ */
+const ResourceEntry = (props) => {
+
+    return (
+        <div>
+            <Accordion.Toggle as={Card.Header} variant="link" eventKey={props.eventKey}>{props.title}</Accordion.Toggle>
+            <Accordion.Collapse eventKey={props.eventKey}>
+            <Card.Body>{props.content}</Card.Body>
+            </Accordion.Collapse>
+        </div>
+    )
+
+}
+
+export default ResourceEntry;
--- src/routes/Resources.js
+++ src/routes/Resources.js
@@ -2,15 +2,53 @@ import React, { Component} from 'react';
 import "../routes/Resources.css";
 import Accordion from 'react-bootstrap/Accordion';
 import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
+import fire from '../fire.js';
+
+// get reference to firebase
+const db = fire.database();
+
 
 class Resources extends Component{
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            titles: [],
+            text: []
+        };
+    }
+
+    getResources() {
+        let dbTitles = [];
+        let dbText = [];
+
+        db.ref('Resources/').once('value', function(snapshot) {
+            snapshot.forEach(function(childSnapshot) {
+                var childKey = childSnapshot.key;
+                dbTitles.push(childKey);
+                console.log("title: " + childKey);
+
+                var childData = childSnapshot.val();
+                dbText.push(childData);
+                console.log("text body: " + childData)
+            })
+
+            // scope is only local
+            console.log(dbTitles);
+            console.log(dbText);
+
+        })
+    }
+
+    componentDidMount() {
+        this.getResources();
+    }
 
     render() {
         return (
                 <div className="base">
                     <h1>Resources</h1>
-                    <p>Get accesses to resources to imrpove in different areas!</p>
+                    <p>Get accesses to resources to improve yourself in different areas!</p>
                     
                     {/* set the defaultActiveKey to whichever section you want to START expanded. */}
                     <Accordion defaultActiveKey="0" className="accordion">

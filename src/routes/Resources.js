dd5d48d1c621d11d1daeba0c465f741857afe52d
Sherry <sherryxrg@outlook.com>
2020-05-04 17:54:38 -0700

    fixed formatting

--- src/routes/Resources.js
+++ src/routes/Resources.js
@@ -8,7 +8,6 @@ import ResourceEntry from "../components/ResourceEntry";
 // get reference to firebase
 const db = fire.database();
 
-
 class Resources extends Component{
 
     state = {
@@ -38,9 +37,7 @@ class Resources extends Component{
                 titles: dbTitles,
                 content: dbContent
             })
-
         })
-
     }
 
     componentDidMount() {
@@ -55,9 +52,7 @@ class Resources extends Component{
                 <div className="base">
                     <h1>Resources</h1>
                     <p>Get accesses to resources to improve yourself in different areas!</p>
-                    
-                    {/* set the defaultActiveKey to whichever section you want to START expanded. */}
-                    <Accordion defaultActiveKey="1" className="accordion">
+                    <Accordion className="accordion">
                         {
                             titles.map((data, index) => (
                                 <ResourceEntry key={index} title={data} content={content[index]} eventKey={index} />
@@ -70,34 +65,4 @@ class Resources extends Component{
 }
 
 
-
 export default Resources;
-
-// <ResourceEntry title="Title 1" content="hello world" eventKey="1" />
-
-// this was the old one that doesn't work because of lack of arrow function!
-    // getResources2() {
-    //     let dbTitles = [];
-    //     let dbContent = [];
-
-    //     db.ref('Resources/').once('value', function(snapshot) {
-    //         snapshot.forEach(function(childSnapshot) {
-    //             var childKey = childSnapshot.key;
-    //             dbTitles.push(childKey);
-    //             console.log("title: " + childKey);
-    //             var childData = childSnapshot.val();
-    //             dbContent.push(childData);
-    //             console.log("text body: " + childData)
-    //         })
-
-    //         console.log(dbTitles);
-    //         console.log(dbContent);
-
-    //         this.setState({
-    //             titles: dbTitles,
-    //             content: dbContent
-    //         })
-
-    //     })
-
-    // }

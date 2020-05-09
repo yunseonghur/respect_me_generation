b5bc625ea335aeb84048bbd7e43a0e99a4ef04cf
hsiehii <hsiehiisuan@gmail.com>
2020-05-08 17:02:56 -0700

    tried displaying two rows of cards on home.js

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -32,90 +32,99 @@ class Home extends React.Component{
     }
     // Get all cards from the users
     async getCards(users){
+        let count = 0;
         let cardsCollected = [];
-        for (let user in users){
-            let cards = users[user].cards
-            for (let card in cards){
-                // Store comments as well if exist
-                if (cards[card]['comments']!=null){
-                    cardsCollected.push({
-                        id: card,
-                        background: cards[card].imgOption,
-                        text: cards[card].text,
-                        comments: cards[card].comments
-                    });
-                } else {
-                    cardsCollected.push({
-                        id: card,
-                        background: cards[card].imgOption,
-                        text: cards[card].text
-                    });
+
+        if(count < 7){
+            for (let user in users){
+                let cards = users[user].cards
+                for (let card in cards){
+                    // Store comments as well if exist
+                    if (cards[card]['comments']!=null){
+                        cardsCollected.push({
+                            id: card,
+                            background: cards[card].imgOption,
+                            text: cards[card].text,
+                            comments: cards[card].comments
+                        });
+                    } else {
+                        cardsCollected.push({
+                            id: card,
+                            background: cards[card].imgOption,
+                            text: cards[card].text
+                        });
+                    }
+                    count = count + 1;
+                    console.log({count});
                 }
             }
         }
         // If the number of total cards is less than 3, display them all
-        if(cardsCollected.length <= 3){
-            this.setState({ cards: cardsCollected });
-            console.log(cardsCollected) // Testing: print all cards stored in the database
-            // Else pick top 3 cards
-        } else {
-        await this.pickTopThreeCards(cardsCollected);
-        }
+        // if(cardsCollected.length <= 3){
+        //     this.setState({ cards: cardsCollected });
+        //     console.log(cardsCollected) // Testing: print all cards stored in the database
+        //     // Else pick top 3 cards
+        // } else {
+        //     // show the first 6
+        //     this.setState({ cards: cardsCollected});
+        //     await this.pickTopThreeCards(cardsCollected);
+        // }
+        this.setState({cards: cardsCollected});
     }
     // Count the number of comments for each card and pick top 3
-    pickTopThreeCards(cardsCollected){
-        let cardsPicked = [];
-        let candidates = [];
-        for(let card in cardsCollected){
-            if(cardsCollected[card]['comments']!=null){
-                candidates.push(cardsCollected[card])
-                candidates.push(Object.keys(cardsCollected[card]['comments']).length)
-            }
-        }
-        let first = -1
-        let second = -1
-        let third = -1
-        let firstCard, secondCard, thirdCard
-        for(var i=1; i<candidates.length; i+=2){
-            /* If current element is greater than first*/
-            if (candidates[i] > first){ 
-                third = second; 
-                second = first; 
-                first = candidates[i]; 
-                // Keep track of their index
-                thirdCard = secondCard;
-                secondCard = firstCard;
-                firstCard = i;
-            /* If candidatesNumOfComments[i] is in between first and second then update second  */
-            } else if (candidates[i] > second){ 
-                third = second; 
-                second = candidates[i]; 
-                thirdCard = secondCard;
-                secondCard = i;
-            } else if (candidates[i] > third) 
-                third = candidates[i]; 
-                thirdCard = i;
-        }
-        // store the most popular card
-        cardsPicked.push({
-            id: candidates[firstCard-1]['id'],
-            background: candidates[firstCard-1]['background'],
-            text: candidates[firstCard-1]['text'],
-        });
-        // store the second most popular card
-        cardsPicked.push({
-            id: candidates[secondCard-1]['id'],
-            background: candidates[secondCard-1]['background'],
-            text: candidates[secondCard-1]['text'],
-        });
-        // store the third most popular card
-        cardsPicked.push({
-            id: candidates[thirdCard-1]['id'],
-            background: candidates[thirdCard-1]['background'],
-            text: candidates[thirdCard-1]['text'],
-        });
-        this.setState({ cards: cardsPicked });
-    }
+    // pickTopThreeCards(cardsCollected){
+    //     let cardsPicked = [];
+    //     let candidates = [];
+    //     for(let card in cardsCollected){
+    //         if(cardsCollected[card]['comments']!=null){
+    //             candidates.push(cardsCollected[card])
+    //             candidates.push(Object.keys(cardsCollected[card]['comments']).length)
+    //         }
+    //     }
+    //     let first = -1
+    //     let second = -1
+    //     let third = -1
+    //     let firstCard, secondCard, thirdCard
+    //     for(var i=1; i<candidates.length; i+=2){
+    //         /* If current element is greater than first*/
+    //         if (candidates[i] > first){ 
+    //             third = second; 
+    //             second = first; 
+    //             first = candidates[i]; 
+    //             // Keep track of their index
+    //             thirdCard = secondCard;
+    //             secondCard = firstCard;
+    //             firstCard = i;
+    //         /* If candidatesNumOfComments[i] is in between first and second then update second  */
+    //         } else if (candidates[i] > second){ 
+    //             third = second; 
+    //             second = candidates[i]; 
+    //             thirdCard = secondCard;
+    //             secondCard = i;
+    //         } else if (candidates[i] > third) 
+    //             third = candidates[i]; 
+    //             thirdCard = i;
+    //     }
+    //     // store the most popular card
+    //     cardsPicked.push({
+    //         id: candidates[firstCard-1]['id'],
+    //         background: candidates[firstCard-1]['background'],
+    //         text: candidates[firstCard-1]['text'],
+    //     });
+    //     // store the second most popular card
+    //     cardsPicked.push({
+    //         id: candidates[secondCard-1]['id'],
+    //         background: candidates[secondCard-1]['background'],
+    //         text: candidates[secondCard-1]['text'],
+    //     });
+    //     // store the third most popular card
+    //     cardsPicked.push({
+    //         id: candidates[thirdCard-1]['id'],
+    //         background: candidates[thirdCard-1]['background'],
+    //         text: candidates[thirdCard-1]['text'],
+    //     });
+    //     this.setState({ cards: cardsPicked });
+    // }
 
     componentDidMount(){
         // this.getDailyMessage();
@@ -132,7 +141,7 @@ class Home extends React.Component{
                 <Quote />
 
                 <div className="card-section">
-                    <h3>Cards<Link to='/communityBoard' className="btn btn-link">> View More</Link></h3>
+                    {/* <h3>Cards<Link to='/communityBoard' className="btn btn-link">> View More</Link></h3> */}
                     <CardDeck>
                         {Array.from(this.state.cards).map((myCard)=> 
                             <MyCard 

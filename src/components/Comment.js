4002ce44afccd99e66bf22fba2469676a21e6b9b
Sherry <sherryxrg@outlook.com>
2020-05-13 16:01:15 -0700

    testing character lengths

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,11 +1,15 @@
 import React, {Component} from 'react';
+import '../components/Comment.css';
 
 class Comment extends Component{
 
     render() {
         return(
-            <div>
-                <h5>{this.props.user} says: {this.props.comment}</h5>
+            <div className="commentWrapper">
+                <div className="bottomBorder">
+                    <h4 className="commenter">{this.props.user}</h4>
+                </div>
+                <h5 className="commentText">{this.props.comment}</h5>
             </div>
         )
     }

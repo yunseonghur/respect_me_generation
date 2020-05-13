94a3fbf6ed2b4f6a855d58f1e8ee217ce48c4d97
Sherry <sherryxrg@outlook.com>
2020-05-12 18:15:49 -0700

    added some styles to cards

--- src/components/MyCard.js
+++ src/components/MyCard.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import Card from 'react-bootstrap/Card';
 import './MyCard.css';
+import { faComments } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 /**
  * Represents a single card object.
@@ -16,6 +18,7 @@ class MyCard extends Component {
                     <Card.Img className="card__img" src={this.props.background} alt="Card image" />
                     <Card.ImgOverlay>
                         <Card.Text className="text-center">{this.props.text}</Card.Text>
+                        <Card.Text className="text-bottom"><FontAwesomeIcon icon={faComments} /></Card.Text>
                     </Card.ImgOverlay>
                 </Card>
                 </a>

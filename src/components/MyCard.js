851c274fc8aa3b54aeb9826f0363bc58c381d21d
Sherry <sherryxrg@outlook.com>
2020-05-14 13:15:27 -0700

    added small margin to prevent cards from smushing together on mobile view

--- src/components/MyCard.js
+++ src/components/MyCard.js
@@ -14,7 +14,7 @@ class MyCard extends Component {
 
     render() {
         return (
-            <div>
+            <div className="cardWrapper">
                 <a style={{ cursor: "pointer" }} onClick={this.props.onClick}>
                 <Card>
                     <Card.Img className="card__img" src={this.props.background} alt="Card image" />
@@ -27,7 +27,6 @@ class MyCard extends Component {
                             <span className="upvoteCount">{this.props.upvoteCount}</span>
                             <FontAwesomeIcon className="thumbs-down" icon={faThumbsDown} />
                             <span className="downvoteCount">{this.props.downvoteCount}</span>
-                            {/* <FontAwesomeIcon onClick={this.iconClick()}className="lightbulb" icon={faLightbulb} /> */}
                         </Card.Text>
                     </Card.ImgOverlay>
                 </Card>
--- src/components/UserVideo.js
+++ src/components/UserVideo.js
@@ -3,7 +3,7 @@ import { CloudinaryContext, Video } from 'cloudinary-react';
 
 const UserVideo = (props) => {
     return (
-        <div style={{display: "inline-block", margin: "10px"}}>
+        <div style={{display: "inline-block", margin: "10px", maxWidth: "400px"}}>
             <CloudinaryContext cloudName="respectmegen">
                 {<Video publicId={props.videoId} width="100%" height="100%" fluid controls></Video>}
             </CloudinaryContext>

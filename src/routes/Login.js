3d14ad47c3b3073b33b3821bccdebffa316f049c
Gina <ginakim.jh@gmail.com>
2020-05-13 21:37:14 -0700

    updated login page

--- src/routes/Login.js
+++ src/routes/Login.js
@@ -37,6 +37,7 @@ class Login extends Component {
     render() {
         return (
             <div className="login">
+                <br /><br /><br /><br /><br /><br /><br /><br />
                 <div id='firebaseui-auth-container' />
             </div>
         )

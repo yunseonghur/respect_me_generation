28c8e18413ae64a0c0f0c1879ced1b2336328952
Gina <ginakim.jh@gmail.com>
2020-05-15 21:58:23 -0700

    redirect to home on login

--- src/routes/Login.js
+++ src/routes/Login.js
@@ -20,7 +20,7 @@ var uiConfig = {
     },
     // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
     // signInFlow: 'popup',
-    signInSuccessUrl: { '/' },
+    signInSuccessUrl: '/',
     signInOptions: [
       // Leave the lines as is for the providers you want to offer your users.
       firebase.auth.GoogleAuthProvider.PROVIDER_ID,

61247a058747195b678c684edadc92c0fb4165ba
Sherry <sherryxrg@outlook.com>
2020-05-17 16:19:33 -0700

    changed some resource icons

--- src/components/ResourceArticles.js
+++ src/components/ResourceArticles.js
@@ -37,7 +37,7 @@ const ARTICLES = [
         title: "10 Things you Can Do to Improve Your Relationship",
         description: "You selected #RELATIONSHIP, you might be interested in this article!",
         link: "https://theeverygirl.com/10-things-you-can-do-to-improve-your-relationship/",
-        image: "https://i.ibb.co/9HdG8z8/Screen-Shot-2020-05-08-at-5-38-13-PM.png"
+        image: "https://i.ibb.co/TYcpt36/rel1.png"
     },
     {
         id: "r2",
@@ -45,7 +45,7 @@ const ARTICLES = [
         title: "The Best Relationship Advice I Ever Received",
         description: "You selected #RELATIONSHIP, you might be interested in this article!",
         link: "https://www.cheatsheet.com/health-fitness/the-best-relationship-advice-i-ever-received.html/",
-        image: "https://i.ibb.co/b3Lz6yM/Screen-Shot-2020-05-09-at-7-09-29-PM.png"
+        image: "https://i.ibb.co/K5S78Rb/rel4.png"
     },
     {
         id: "r3",
@@ -53,7 +53,7 @@ const ARTICLES = [
         title: "Teenage relationships: romance and intimacy",
         description: "You selected #RELATIONSHIP, you might be interested in this article!",
         link: "https://raisingchildren.net.au/pre-teens/communicating-relationships/romantic-relationships/teen-relationships",
-        image: "https://i.ibb.co/BP3MfTN/Screen-Shot-2020-05-09-at-7-28-33-PM.png"
+        image: "https://i.ibb.co/0fM8x73/rel3.png"
     },
     {
         id: "r4",
@@ -61,7 +61,7 @@ const ARTICLES = [
         title: "Am I in a Healthy Relationship?",
         description: "You selected #RELATIONSHIP, you might be interested in this article!",
         link: "https://kidshealth.org/en/teens/healthy-relationship.html",
-        image: "https://www.akronchildrens.org/kidshealth/EN/images/headers/T-healthyRelationship-enHD-AR1.jpg"
+        image: "https://i.ibb.co/LkhQ1sR/rel2.png"
     },
     {
         id: "h1",
@@ -69,7 +69,7 @@ const ARTICLES = [
         title: "Practicing Self-Respect: 9 Effective Strategies",
         description: "You selected #HEALTH, you might be interested in this article!",
         link: "https://www.liveyourtruestory.com/practicing-self-respect-9-effective-strategies-confidence/",
-        image: "https://eastside-online.org/wp-content/uploads/2019/11/depositphotos_103164650-stock-illustration-respect-concept-heart-900x675.jpg"
+        image: "https://i.ibb.co/pjXPGR7/health1.jpg"
     },
     {
         id: "h2",
@@ -85,7 +85,7 @@ const ARTICLES = [
         title: "How to Manage Your Mental Health",
         description: "You selected #HEALTH, you might be interested in this article!",
         link: "https://www.glassdoor.com/blog/how-to-manage-your-mental-health-as-a-student-with-a-stressful-job/",
-        image: "https://www.glassdoor.com/blog/app/uploads/sites/2/iStock-626647986-1024x450.jpg"
+        image: "https://i.ibb.co/8XS7Ty7/health3.png"
     },
     {
         id: "h4",
@@ -93,7 +93,7 @@ const ARTICLES = [
         title: "How to Eat Healthy",
         description: "You selected #HEALTH, you might be interested in this article!",
         link: "https://www.hhs.gov/fitness/eat-healthy/how-to-eat-healthy/index.html",
-        image: "https://www.hhs.gov/sites/default/files/assorted-vegetables.jpg"
+        image: "https://i.ibb.co/x7WFwv8/health4.jpg"
     }
 
 ];

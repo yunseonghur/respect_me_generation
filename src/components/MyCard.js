5c51314ae5a1dc7ffd38e591ea77312ecd788e0e
Gina <ginakim.jh@gmail.com>
2020-05-17 14:46:34 -0700

    aligned card text

--- src/components/MyCard.js
+++ src/components/MyCard.js
@@ -18,7 +18,7 @@ class MyCard extends Component {
                 <Card onClick={this.props.onClick}>
                     <Card.Img className="card__img" src={this.props.background} alt="Card image" />
                     <Card.ImgOverlay className="overlay">
-                        <Card.Text className="text-center">{this.props.text}</Card.Text>
+                        <Card.Text>{this.props.text}</Card.Text>
                         <Card.Text className="text-bottom-left">
                             <FontAwesomeIcon icon={faComments} />
                             <span className="commentCount iconItem">{this.props.commentCount}</span>

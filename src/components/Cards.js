da41c62008c8051d2467d11c4f1f803c333e1a82
Gina <ginakim.jh@gmail.com>
2020-09-28 13:13:16 -0700

    Update classnames in board js

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,240 +1,246 @@
-import React from 'react';
-import MyCard from './MyCard';
-import {CardDeck, Container} from 'react-bootstrap';
-import AddComment from './AddComment';
-import fire from '../fire'
-import loading from '../images/loading.gif';
+import React from "react";
+import MyCard from "./MyCard";
+import { CardDeck } from "react-bootstrap";
+import AddComment from "./AddComment";
+import fire from "../fire";
+import loading from "../images/loading.gif";
+import "./Cards.css";
 const dbRef = fire.database().ref();
 
 /**
  * A layout displaying cards from all users.
  * Called in Board.js and MiniBoard.js
  */
-class Cards extends React.Component{
+class Cards extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cardSelected: "",
+      isLoading: true,
+      showCards: true,
+      visible: false,
+      cards: [],
+      tag: "",
+    };
+  }
 
-    constructor(props){
-        super(props);
-        this.state = {
-            cardSelected: "",
-            isLoading: true,
-            showCards: true, 
-            visible: false,
-            cards: [],
-            tag: ""
-        }
-    }
+  /**
+   * Grabs all users' cards from firebase
+   * @param {firebaseUser} users
+   */
+  getCards(users) {
+    let cardCollected = [];
+    for (let user in users) {
+      let cards = users[user].cards;
 
-    /**
-     * Grabs all users' cards from firebase
-     * @param {firebaseUser} users 
-     */
-    getCards(users){
-        let cardCollected = [];
-        for (let user in users){
-            let cards = users[user].cards
-
-            for (let card in cards){
-                cardCollected.push({
-                    id: card,
-                    background: cards[card].imgOption,
-                    text: cards[card].text,
-                    comments: cards[card].comments,
-                    timestamp: cards[card].timestamp
-                });
-            }
-        }
-        this.setState({ 
-            cards: cardCollected,
-            isLoading: false
-        })
-    }
-
-    componentDidMount(){
-        dbRef.child('User').on('value', snap => {
-            const users = snap.val();
-            this.getCards(users);
+      for (let card in cards) {
+        cardCollected.push({
+          id: card,
+          background: cards[card].imgOption,
+          text: cards[card].text,
+          comments: cards[card].comments,
+          timestamp: cards[card].timestamp,
         });
+      }
     }
+    this.setState({
+      cards: cardCollected,
+      isLoading: false,
+    });
+  }
 
-    static getDerivedStateFromProps(props, state) {
-        return {tag: props.tag}
-    }
+  componentDidMount() {
+    dbRef.child("User").on("value", (snap) => {
+      const users = snap.val();
+      this.getCards(users);
+    });
+  }
 
-    /**
-     * Visible state setter when card is clicked
-     */
-    cardClicked = () => {
-        this.setState({visible: true})
-      };
-    
-    /**
-     * Visible state setter when card is closed
-     */
-    hideModal = () => {
-        this.setState({visible: false})
-    }
+  static getDerivedStateFromProps(props, state) {
+    return { tag: props.tag };
+  }
 
-    /**
-     * Returns the Google UID of card owner
-     */
-    getCardOwner = () => {
-        let cardOwnerUID;
-        dbRef.child('User').on('value', snap => {
-            const users = snap.val();
-            for (let user in users){
-                let cards = users[user].cards
-                for (let card in cards){
-                    if(card === this.state.cardSelected){
-                        cardOwnerUID = user
-                    }
-                }
-            }
-        });
-        return cardOwnerUID
-      };
+  /**
+   * Visible state setter when card is clicked
+   */
+  cardClicked = () => {
+    this.setState({ visible: true });
+  };
 
-    /**
-     * counts the number of comments a user has.
-     * @param {Comment} cardCommentObj a card comment object stored in user
-     */
-    countComments = (cardCommentObj) => {
-        // count comments under each card
-        let cardComment = cardCommentObj;
-        let commentNumber = 0;
-        if (cardComment != null) {
-            // count and increment commentNumber
-            for (let count in cardComment) {
-                commentNumber++;
-            }
+  /**
+   * Visible state setter when card is closed
+   */
+  hideModal = () => {
+    this.setState({ visible: false });
+  };
+
+  /**
+   * Returns the Google UID of card owner
+   */
+  getCardOwner = () => {
+    let cardOwnerUID;
+    dbRef.child("User").on("value", (snap) => {
+      const users = snap.val();
+      for (let user in users) {
+        let cards = users[user].cards;
+        for (let card in cards) {
+          if (card === this.state.cardSelected) {
+            cardOwnerUID = user;
+          }
         }
+      }
+    });
+    return cardOwnerUID;
+  };
 
-        return commentNumber;
+  /**
+   * counts the number of comments a user has.
+   * @param {Comment} cardCommentObj a card comment object stored in user
+   */
+  countComments = (cardCommentObj) => {
+    // count comments under each card
+    let cardComment = cardCommentObj;
+    let commentNumber = 0;
+    if (cardComment != null) {
+      // count and increment commentNumber
+      for (let count in cardComment) {
+        commentNumber++;
+      }
     }
-    
-    /**
-     * Sorts cards by tag
-     */ 
-    sortByTag() {
-        var users;
-        dbRef.child('User').on('value', snap => {
-            users = snap.val();
-        });
-        let cardCollected = [];
-        for (let user in users){
-            
-            let cards = users[user].cards
 
-            if (this.state.tag !== "all") {
-                for (let card in cards){
-                    let commentNumber = this.countComments(cards[card].comments)
+    return commentNumber;
+  };
 
-                    if (cards[card].tag === this.state.tag) {
-                        cardCollected.push({
-                            id: card,
-                            background: cards[card].imgOption,
-                            text: cards[card].text,
-                            comments: cards[card].comments,
-                            numComments: commentNumber,
-                            timestamp: cards[card].timestamp,
-                            upvote: this.countUpvotes(cards[card].upvote),
-                            downvote: this.countDownvotes(cards[card].downvote)
-                        });
-                    } 
-                }
-            } else if (this.props.tag === "all") {
-                for (let card in cards){
+  /**
+   * Sorts cards by tag
+   */
+  sortByTag() {
+    var users;
+    dbRef.child("User").on("value", (snap) => {
+      users = snap.val();
+    });
+    let cardCollected = [];
+    for (let user in users) {
+      let cards = users[user].cards;
 
-                    let commentNumber = this.countComments(cards[card].comments)
+      if (this.state.tag !== "all") {
+        for (let card in cards) {
+          let commentNumber = this.countComments(cards[card].comments);
 
-                    cardCollected.push({
-                        id: card,
-                        background: cards[card].imgOption,
-                        text: cards[card].text,
-                        comments: cards[card].comments,
-                        numComments: commentNumber,
-                        timestamp: cards[card].timestamp,
-                        upvote: this.countUpvotes(cards[card].upvote),
-                        downvote: this.countDownvotes(cards[card].downvote)
-                    });
-                }
-            }
-
+          if (cards[card].tag === this.state.tag) {
+            cardCollected.push({
+              id: card,
+              background: cards[card].imgOption,
+              text: cards[card].text,
+              comments: cards[card].comments,
+              numComments: commentNumber,
+              timestamp: cards[card].timestamp,
+              upvote: this.countUpvotes(cards[card].upvote),
+              downvote: this.countDownvotes(cards[card].downvote),
+            });
+          }
         }
-        this.sortByTimestamp(cardCollected);
-        return cardCollected;
-    }
+      } else if (this.props.tag === "all") {
+        for (let card in cards) {
+          let commentNumber = this.countComments(cards[card].comments);
 
-    /**
-     * Sorts cards from users by timestamp
-     * @param {array} cardCollected a list of all cards from iterating through all users
-     */ 
-    sortByTimestamp(cardCollected){
-        cardCollected.sort(function (a, b){
-            return b.timestamp - a.timestamp;
-        })
-        return cardCollected;
-    }
-
-    /**
-     * Return count of upVote
-     * @param {int} upVotes
-     * */ 
-    countUpvotes = (upVotes) => {
-        if (upVotes != null) {
-            return upVotes;
+          cardCollected.push({
+            id: card,
+            background: cards[card].imgOption,
+            text: cards[card].text,
+            comments: cards[card].comments,
+            numComments: commentNumber,
+            timestamp: cards[card].timestamp,
+            upvote: this.countUpvotes(cards[card].upvote),
+            downvote: this.countDownvotes(cards[card].downvote),
+          });
         }
-            return "0";
+      }
     }
+    this.sortByTimestamp(cardCollected);
+    return cardCollected;
+  }
 
-    /**
-     * Return count of downVote
-     * @param {int} downVotes
-     */
-    countDownvotes = (downVotes) => {
-        if (downVotes != null) {
-            return downVotes;
-        }
-            return "0";
-    }
+  /**
+   * Sorts cards from users by timestamp
+   * @param {array} cardCollected a list of all cards from iterating through all users
+   */
+  sortByTimestamp(cardCollected) {
+    cardCollected.sort(function (a, b) {
+      return b.timestamp - a.timestamp;
+    });
+    return cardCollected;
+  }
 
-    render() {
-        return (
-            <div>
-                {this.state.isLoading ? (
-                    <div className="loader">
-                    <span className="loader__text"><br/><br/><br/><img src={loading} alt="loading"/></span>
-                    </div>
-                ) : this.state.showCards ?
-                    (
-                    <Container>
-                        <CardDeck className="row row-cols-sm-2 row-cols-md-3">
-                        {Array.from(this.sortByTag()).map((card)=> 
-                            <div key={card.id}>
-                            <MyCard
-                                key={card.id} 
-                                id={card.id} 
-                                background={card.background} 
-                                text={card.text} 
-                                commentCount={card.numComments}
-                                upvoteCount={card.upvote}
-                                downvoteCount={card.downvote}
-                                onClick={()=>{
-                                    this.setState({ visible: true, cardSelected: card.id });
-                                }}
-                            /> </div>)}
-                            {this.state.visible ?
-                            <AddComment show={this.state.visible} cardOwnerUID={this.getCardOwner()} cardID={this.state.cardSelected} onHide={() => this.setState({visible: false})}/>
-                            : null}
-                        </CardDeck>
-                    </Container>
-                    ) : (
-                        <div className="loader">
-                        <span className="loader__text">Not Visible</span>
-                        </div>
-                    )}
-            </div>
-        )
+  /**
+   * Return count of upVote
+   * @param {int} upVotes
+   * */
+  countUpvotes = (upVotes) => {
+    if (upVotes != null) {
+      return upVotes;
     }
+    return "0";
+  };
+
+  /**
+   * Return count of downVote
+   * @param {int} downVotes
+   */
+  countDownvotes = (downVotes) => {
+    if (downVotes != null) {
+      return downVotes;
+    }
+    return "0";
+  };
+
+  render() {
+    return (
+      <div className="cards">
+        {this.state.isLoading ? (
+          <div className="cards__loader">
+            <span className="cards__loader--text">
+              <br />
+              <br />
+              <br />
+              <img src={loading} alt="loading" />
+            </span>
+          </div>
+        ) : this.state.showCards ? (
+          <CardDeck className="cards__carddeck row">
+            {Array.from(this.sortByTag()).map((card) => (
+              <div key={card.id}>
+                <MyCard
+                  key={card.id}
+                  id={card.id}
+                  background={card.background}
+                  text={card.text}
+                  commentCount={card.numComments}
+                  upvoteCount={card.upvote}
+                  downvoteCount={card.downvote}
+                  onClick={() => {
+                    this.setState({ visible: true, cardSelected: card.id });
+                  }}
+                />{" "}
+              </div>
+            ))}
+            {this.state.visible ? (
+              <AddComment
+                show={this.state.visible}
+                cardOwnerUID={this.getCardOwner()}
+                cardID={this.state.cardSelected}
+                onHide={() => this.setState({ visible: false })}
+              />
+            ) : null}
+          </CardDeck>
+        ) : (
+          <div className="cards__loader">
+            <span className="cards__loader--text">Not Visible</span>
+          </div>
+        )}
+      </div>
+    );
+  }
 }
 
 export default Cards;
--- src/components/MyCard.js
+++ src/components/MyCard.js
@@ -1,15 +1,14 @@
-import React, {Component} from 'react';
-import PropTypes from 'prop-types';
-import Card from 'react-bootstrap/Card';
-import './MyCard.css';
-import { faComments, faThumbsUp, faThumbsDown } from "@fortawesome/free-solid-svg-icons";
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import Card from "react-bootstrap/Card";
+import "./MyCard.css";
+import { faComments, faThumbsUp } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
 
 /**
  * Represents a single card object.
  * Called in CardModal.js, Cards.js, AddComment.js, Profile.js
- * 
+ *
  * @param {function} onClick should show the AddComment modal
  * @param {string} background
  * @param {int} commentCount
@@ -17,34 +16,30 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
  * @param {int} downvoteCount
  */
 class MyCard extends Component {
-
-    render() {
-        return (
-            <div className="cardWrapper">
-                <Card onClick={this.props.onClick}>
-                    <Card.Img className="card__img" src={this.props.background} alt="Card image" />
-                    <Card.ImgOverlay className="overlay">
-                        <Card.Text className="text-center">{this.props.text}</Card.Text>
-                        <Card.Text className="text-bottom-left">
-                            <FontAwesomeIcon icon={faComments} />
-                            <span className="commentCount iconItem">{this.props.commentCount}</span>
-                            <FontAwesomeIcon className="thumbs-up" icon={faThumbsUp} />
-                            <span className="upvoteCount iconItem">{this.props.upvoteCount}</span>
-                            <FontAwesomeIcon className="thumbs-down" icon={faThumbsDown} />
-                            <span className="downvoteCount iconItem">{this.props.downvoteCount}</span>
-                        </Card.Text>
-                    </Card.ImgOverlay>
-                </Card>
-            </div>
-        )
-
-    }
+  render() {
+    return (
+      <div className="mycard">
+        <Card className="mycard__card" onClick={this.props.onClick}>
+          <Card.Img className="mycard__card--img" src={this.props.background} alt="Card image" />
+          <Card.ImgOverlay className="mycard__card__overlay">
+            <Card.Text className="mycard__card__overlay--text">{this.props.text}</Card.Text>
+            <Card.Text className="mycard__card__overlay--textbottom">
+              <FontAwesomeIcon icon={faComments} />
+              <span className="commentCount iconItem">{this.props.commentCount}</span>
+              <FontAwesomeIcon className="thumbs-up" icon={faThumbsUp} />
+              <span className="upvoteCount iconItem">{this.props.upvoteCount}</span>
+            </Card.Text>
+          </Card.ImgOverlay>
+        </Card>
+      </div>
+    );
+  }
 }
 
 MyCard.propTypes = {
-    id: PropTypes.string,
-    background: PropTypes.string.isRequired,
-    text: PropTypes.string.isRequired
-}
+  id: PropTypes.string,
+  background: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+};
 
 export default MyCard;

1bf1014fb37f9beb3906f9a3081f406a5b829051
Sherry <sherryxrg@outlook.com>
2020-10-07 22:21:32 -0700

    grid not quite working, saved progress

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -248,7 +248,16 @@ class Profile extends Component{
                             />
                         </div>
                     </TabPanel><TabPanel tabId="badges">
-                        <p>Tab 4 content</p>
+                        <div className="profile_badges-grid">
+                            <div>1</div>
+                            <div>2</div>
+                            <div>3</div>
+                            <div>4</div>
+                            <div>5</div>
+                            <div>6</div>
+                            <div>7</div>
+                            <div>8</div>
+                        </div>
                     </TabPanel>
                 </Tabs>
             </div>  // closing root node

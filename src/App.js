15ca4ac628737d137b9b40e6ef44f32bfe66a79c
Gina <ginakim.jh@gmail.com>
2020-11-15 19:45:05 -0800

    Show dashboard only when user is logged in

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { HashRouter, Route } from "react-router-dom";
 import CommunityBoard from "./routes/CommunityBoard";
 import Resources from "./routes/Resources";
@@ -8,12 +8,31 @@ import Navigation from "./components/Navigation";
 import CreateCard from "./routes/CreateCard";
 import Dashboard from "./routes/Dashboard";
 import "./App.css";
+import fire from "./fire.js";
 
 function App() {
+  const [user, setUser] = useState(null);
+  useEffect(() => {
+    authListener();
+  });
+  /**
+   * Authentication listener
+   */
+  const authListener = () => {
+    fire.auth().onAuthStateChanged((user) => {
+      if (user) {
+        setUser(user);
+      }
+    });
+  };
   return (
     <HashRouter>
       <Navigation />
-      <Route path="/" exact={true} component={Login} />
+      {user ? (
+        <Route path="/" exact={true} component={Dashboard} />
+      ) : (
+        <Route path="/" exact={true} component={Login} />
+      )}
       <Route path="/communityBoard" component={CommunityBoard} />
       <Route path="/resources" component={Resources} />
       <Route path="/profile" component={Profile} />
